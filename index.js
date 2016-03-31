9609e2c4da3b286e4c3e726c73d6de7179286a91
seriousManual <seriousstuff@gmx.de>
2016-03-31 13:12:34 +0200

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+function wr(entriesMap) {
+    var weightSum = Array
+        .from(entriesMap.values())
+        .reduce((carray, value) => carray + value, 0)
+
+    var choosenEntry = null
+    var runningSum = 0
+    var randomPointer = Math.random() * weightSum
+
+    Array
+        .from(entriesMap.entries())
+        .some(entry => {
+            runningSum = runningSum + entry[1]
+
+            if (randomPointer < runningSum) {
+                choosenEntry = entry[0]
+                return true
+            }
+
+            return false
+        })
+
+    return choosenEntry
+}
+
+module.exports = wr
---
+++ test.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect
+
+var wr = require('./')
+
+var dishes = {todo: 'do the dishes'}
+var laundry = {todo: 'do the laundry'}
+var rubbish = {todo: 'put the rubbish out'}
+var procrastinate = {todo: 'procrastinate'}
+
+var todos = new Map()
+todos.set(dishes, 0.1)
+todos.set(laundry, 0.1)
+todos.set(rubbish, 0.1)
+todos.set(procrastinate, 0.7)
+
+var countRuns = 100000
+var testMap = new Map()
+for (var i = 0; i < countRuns; i++) {
+    var rez = wr(todos)
+    if (!testMap.has(rez)) {
+        testMap.set(rez, 1)
+    } else {
+        testMap.set(rez, testMap.get(rez) + 1)
+    }
+}
+
+expect(testMap.get(dishes) / countRuns * 100).to.be.above(9)
+expect(testMap.get(dishes) / countRuns * 100).to.be.below(11)
+
+expect(testMap.get(laundry) / countRuns * 100).to.be.above(9)
+expect(testMap.get(laundry) / countRuns * 100).to.be.below(11)
+
+expect(testMap.get(rubbish) / countRuns * 100).to.be.above(9)
+expect(testMap.get(rubbish) / countRuns * 100).to.be.below(11)
+
+expect(testMap.get(procrastinate) / countRuns * 100).to.be.above(69)
+expect(testMap.get(procrastinate) / countRuns * 100).to.be.below(71)
+
+console.log('kthxbye')
