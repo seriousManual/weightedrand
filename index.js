afc663d225950307906f9fcba0967e6727a30fba
seriousManual <seriousstuff@gmx.de>
2016-06-01 12:13:52 +0200

    improve implementation

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 function weightedrand(entriesMap) {
     var entries = Array.from(entriesMap.entries())
 
-    var choosenEntry = null
     var runningSum = 0
     var weightSum = entries.reduce((carry, value) => carry + value[1], 0)
     var randomPointer = Math.random() * weightSum
 
-    entries
-        .some(entry => {
-            runningSum = runningSum + entry[1]
+    for (var i = 0; i < entries.length; i++) {
+        var entry = entries[i]
 
-            if (randomPointer < runningSum) {
-                choosenEntry = entry[0]
-                return true
-            }
+        runningSum = runningSum + entry[1]
 
-            return false
-        })
+        if (randomPointer < runningSum) {
+            return entry[0]
+        }
+    }
 
-    return choosenEntry
+    return null
 }
 
 module.exports = weightedrand
